e63a1d705e16a86bca00d02de042be94f15c3bc3
otetLopez <otet.tud@gmail.com>
2020-01-10 16:28:53 -0500

    Creates PolyLines and Circles

--- OverlayDemo/ViewController.swift
+++ OverlayDemo/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    let places = Place.getPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        //locationManager.startUpdatingLocation()
+        
+        addAnnotations()
+        addPolyLine()
     }
-
-
+    
+    func addAnnotations() {
+        mapView.delegate = self
+        mapView.addAnnotations(places)
+        
+        // Draw a circle with a radius of 100 for each annotations
+        // To make this work add render for overlays in extension
+        let overlays = places.map { (MKCircle(center: $0.coordinate, radius: 10000)) }
+        mapView.addOverlays(overlays)
+    }
+    
+    // This function will create lines between location
+    func addPolyLine() {
+        let locations = places.map {$0.coordinate}
+        let polyLine = MKPolyline(coordinates: locations, count: locations.count)
+        mapView.addOverlay(polyLine)
+    }
 }
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        } else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "ic_place")
+            return annotationView
+        }
+    }
+    
+    // This function is needed to add overlays
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            // Cutomize overlay
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            renderer.strokeColor = UIColor.green
+            renderer.lineWidth = 2
+            return renderer
+        } else if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.blue
+            renderer.lineWidth = 3
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
+    
+}
